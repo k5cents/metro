37345f2a6f096b653885c2006e1e1f8862a81a95
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-13 23:20:01 -0500

    Return all line stations

---
+++ R/stations.R
@@ -0,0 +1,35 @@
+#' Metro rail stations
+#'
+#' The stations on a Metro rail line, including the full names and geographic
+#' locations. The four big transfer stations have two station code designations,
+#' one for each track direction. All the lines running through each station
+#' are listed in a single list-column.
+#'
+#' @details
+#' The `line_code` takes one of these two-letter codes:
+#' * BL = Blue
+#' * GR = Green
+#' * OR = Orange
+#' * RD = Red
+#' * SV = Silver
+#' * YL = Yellow
+#'
+#' The result of this endpoint is saved as the [stations] object.
+#' @param line Two-letter line code abbreviation, see Details or [rail_lines()].
+#'   If `NULL`, (the default) all stations are returned.
+#' @export
+rail_stations <- function(line = NULL) {
+  json <- wmata_api("Rail.svc/json/jStations", list(LineCode = line))
+  df <- jsonlite::fromJSON(json, flatten = TRUE)
+  df <- type.convert(df$Stations[, c(-4, -8)], na.strings = "", as.is = TRUE)
+  l <- mapply(paste, df[4], df[5], df[6], SIMPLIFY = FALSE)
+  l <- strsplit(gsub("\\s?NA\\s?", "", l[[1]]), "\\s")
+  df$lines <- l
+  df <- df[, c(1:3, 13, 7:12)]
+  nm <- names(df)
+  nm[c(1, 3)] <- c("station", "txfer")
+  names(df) <- gsub("address\\.", "", tolower(nm))
+  tibble::as_tibble(df)
+}
+
+
---
+++ tests/testthat/test-stations.R
@@ -0,0 +1,17 @@
+Sys.sleep(1)
+test_that("rail stations are listed for all lines", {
+  s <- rail_stations(line = NULL)
+  expect_length(s, 10)
+  expect_equal(nrow(s), 95)
+  expect_s3_class(s, "tbl")
+  expect_type(s$lines, "list")
+})
+
+Sys.sleep(1)
+test_that("rail stations are listed for one", {
+  s <- rail_stations(line = "RD")
+  expect_length(s, 10)
+  expect_equal(nrow(s), 27)
+  expect_s3_class(s, "tbl")
+  expect_type(s$lines, "list")
+})
