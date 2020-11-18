2feca2c6dfd360a55857b427ac67c22356567a17
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-18 16:00:17 -0500

    Cover bus position endpoints
    Progress on #1

---
+++ R/bus-position.R
@@ -0,0 +1,71 @@
+#' Bus position
+#'
+#' Returns bus positions for the given route, with an optional search radius. If
+#' no parameters are specified, all bus positions are returned.
+#'
+#' Note that the `route` parameter accepts only base route names and no
+#' variations, i.e.: use 10A instead of 10Av1 or 10Av2.
+#' @format A tibble with 13 variables:
+#' \describe{
+#'   \item{vehicle}{Unique identifier. Usually visible on the bus itself.}
+#'   \item{lat}{Last reported Latitude of the bus.}
+#'   \item{lon}{Last reported Longitude of the bus.}
+#'   \item{dist}{Distance in meters from provided coordinates.}
+#'   \item{deviation}{Deviation, in minutes, from schedule.}
+#'   \item{time}{Date and time (EST) of last position update.}
+#'   \item{trip}{Unique trip ID. Used with the schedule-related functions.}
+#'   \item{route}{Base route name as shown on the bus.}
+#'   \item{direction}{General direction of the trip, not the bus itself }
+#'   \item{head}{Destination of the bus.}
+#'   \item{start}{Scheduled start time (EST) of the bus's current trip.}
+#'   \item{end}{Scheduled end time (EST) of the bus's current trip.}
+#'   \item{block}{}
+#' }
+#' @source <https://api.wmata.com/Bus.svc/json/jBusPositions>
+#' @param route Base bus route, e.g.: 70, 10A.
+#' @param lat Center point Latitude.
+#' @param lon Center point Longitude.
+#' @param radius Meters to include in the search area for `lat` and `lon`.
+#' @export
+bus_position <- function(route = NULL, lat = NULL, lon = NULL, radius = 1000) {
+  coord <- list(Lat = lat, Lon = lon, Radius = radius)
+  json <- wmata_api(
+    type = "Bus", endpoint = "jBusPositions",
+    query = c(list(RouteId = route), coord)
+  )
+  df <- jsonlite::fromJSON(json, flatten = TRUE)[[1]]
+  if (length(df) == 0) {
+    warning("no routes found within your radius, please expand")
+    return(empty_routes)
+  }
+  df <- df[, -8]
+  names(df) <- names(empty_routes)[-4]
+  df[c(5, 10:11)] <- lapply(df[c(5, 10:11)], api_time)
+  dist <- if (is.null(lat) || is.null(lon)) {
+    NA_real_
+  } else {
+    as.vector(geodist::geodist(coord, df))
+  }
+  df <- tibble::add_column(df, distance = dist, .after = "lon")
+  tibble::as_tibble(df)
+}
+
+api_time <- function(x) {
+  as.POSIXlt(x, format = "%Y-%m-%dT%H:%M:%S")
+}
+
+empty_routes <- data.frame(
+  vehicle = character(),
+  lat = double(),
+  lon = double(),
+  distance = double(),
+  deviation = double(),
+  time = as.POSIXlt(character()),
+  trip = character(),
+  route = character(),
+  direction = character(),
+  head = character(),
+  start = as.POSIXlt(character()),
+  end = as.POSIXlt(character()),
+  block = character()
+)
---
+++ tests/testthat/test-bus-positions.R
@@ -0,0 +1,37 @@
+test_that("entrace returns empty and warns for short radius", {
+  skip_if_no_key()
+  Sys.sleep(0.1)
+  e <- expect_warning(bus_position(NULL, 38, -77, 0))
+  expect_length(e, 13)
+  expect_s3_class(e, "data.frame")
+  expect_equal(nrow(e), 0)
+})
+
+test_that("entrances found for single location", {
+  skip_if_no_key()
+  Sys.sleep(0.1)
+  # white house
+  e <- bus_position(NULL, 38.897957, -77.036560, 1000)
+  expect_length(e, 13)
+  expect_s3_class(e, "data.frame")
+  expect_false(any(is.na(e$distance)))
+})
+
+test_that("all entrances returned without radius", {
+  skip_if_no_key()
+  Sys.sleep(0.1)
+  # white house
+  e <- bus_position(NULL, 38.897957, -77.036560)
+  expect_length(e, 13)
+  expect_s3_class(e, "data.frame")
+  expect_false(any(is.na(e$distance)))
+})
+
+test_that("distances not returned without coordinates", {
+  skip_if_no_key()
+  Sys.sleep(0.1)
+  e <- bus_position()
+  expect_length(e, 13)
+  expect_s3_class(e, "data.frame")
+  expect_true(all(is.na(e$distance)))
+})
