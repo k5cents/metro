313673db3a3f81be0cd21707d25ad34b50365dac
Kiernan Nicholls <kiernann@protonmail.com>
2021-02-14 13:07:02 -0500

    Use WMATA real-time names and structure

--- R/next-bus.R
+++ R/next-bus.R
@@ -1,27 +1,41 @@
-#' Next buses
+#' Next Buses
 #'
 #' Returns next bus arrival times at a stop.
 #'
-#' @format A tibble with 6 variables:
+#' @format A tibble 1 row per arrival with 6 variables:
 #' \describe{
-#'   \item{route}{Base route name as shown on the bus.}
-#'   \item{direction}{Friendly description of direction and destination.}
-#'   \item{dir_num}{Denotes a binary direction (0 or 1) of the bus. There is no
-#'   specific mapping to direction, but a different value for the same route
-#'   signifies that the buses are traveling in opposite directions.}
-#'   \item{minutes}{Minutes until bus arrival at this stop.}
-#'   \item{vehicle}{Bus identifier.}
-#'   \item{trip}{Trip identifier.}
+#'   \item{RouteID}{Base route name as shown on the bus. This can be used in
+#'   other bus-related methods. Note that all variants will be shown as their
+#'   base route names (i.e.: 10Av1 and 10Av2 will be shown as 10A).}
+#'   \item{DirectionText}{Customer-friendly description of direction and
+#'   destination for a bus.}
+#'   \item{DirectionNum}{Denotes a binary direction (0 or 1) of the bus. There
+#'   is no specific mapping to direction, but a different value for the same
+#'   route signifies that the buses are traveling in opposite directions. Use
+#'   the DirectionText element to show the actual destination of the bus.}
+#'   \item{Minutes}{Minutes until bus arrival at this stop. Numeric value.}
+#'   \item{VehicleID}{Bus identifier. This can be correlated with results
+#'   returned from bus positions.}
+#'   \item{TripID}{Trip identifier. This can be correlated with the data in our
+#'   bus schedule information as well as bus positions.}
 #' }
-#' @param stop 7-digit regional stop ID.
+#' @param StopID 7-digit regional stop ID.
+#' @examples
+#' \dontrun{
+#' next_bus(stopID = "1001195")
+#' }
+#' @return Data frame of bus arrivals
+#' @seealso <https://developer.wmata.com/docs/services/5476365e031f590f38092508/operations/5476365e031f5909e4fe331d>
+#' @family Real-Time Predictions
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
 #' @export
-next_bus <- function(stop = NULL) {
+next_bus <- function(StopID = NULL) {
   json <- wmata_api(
     type = "NextBusService",
     endpoint = "jPredictions",
-    query = list(StopID = stop)
+    query = list(StopID = StopID)
   )
-  df <- jsonlite::fromJSON(json, flatten = TRUE)[[2]]
-  names(df) <- c("route", "direction", "dir_num", "minutes", "vehicle", "trip")
-  tibble::as_tibble(df)
+  dat <- jsonlite::fromJSON(json, flatten = TRUE)[[2]]
+  tibble::as_tibble(dat)
 }
--- R/next-train.R
+++ R/next-train.R
@@ -1,9 +1,11 @@
-#' Next train
+#' Next Trains
 #'
+#' @description
 #' Returns next train arrival information for one or more stations. Will return
-#' an empty set of results when no predictions are available. Use `NULL` for the
-#' `station` parameter to return predictions for all stations.
+#' an empty set of results when no predictions are available. Use "All" for the
+#' `StationCodes` parameter to return predictions for all stations.
 #'
+#' @details
 #' For terminal stations (e.g.: Greenbelt, Shady Grove, etc.), predictions may
 #' be displayed twice.
 #'
@@ -11,57 +13,78 @@
 #' Plaza, and Metro Center). To retrieve complete predictions for these
 #' stations, be sure to pass in both StationCodes.
 #'
-#' For trains with no passengers, the DestinationName will be "No Passenger".
-#' @format A tibble with 9 variables:
+#' For trains with no passengers, the `DestinationName` will be "No Passenger".
+#'
+#' @format A tibble 1 row per arrival with 9 variables:
 #' \describe{
-#'   \item{order}{The order in which trains will arrive at the station.}
-#'   \item{station}{Station code for where the train is arriving.}
-#'   \item{name}{Full name of the station where the train is arriving.}
-#'   \item{min}{Minutes until arrival: "ARR" converted to 0 and "BRD" to -1.}
-#'   \item{to_station}{Abbreviated _final_ destination for a train.}
-#'   \item{to_name}{Destination station code.}
-#'   \item{group}{Denotes the track this train is on but not Track "1" or "2".}
-#'   \item{line}{Two-letter abbreviation for the line}
-#'   \item{cars}{Number of cars on a train, usually 6 or 8.}
+#'   \item{Car}{Number of cars on a train, usually 6 or 8, but might also `NA`.}
+#'   \item{Destination}{Abbreviated version of the final destination for a
+#'   train. This is similar to what is displayed on the signs at stations.}
+#'   \item{DestinationCode}{}Destination station code. Can be `NA`. Use this
+#'   value in other rail-related APIs to retrieve data about a station.
+#'   \item{DestinationName}{When `DestinationCode` is populated, this is the
+#'   full name of the destination station, as shown on the WMATA website.}
+#'   \item{Group}{Denotes the track this train is on, but does not necessarily
+#'   equate to Track 1 or Track 2. With the exception of terminal stations,
+#'   predictions at the same station with different Group values refer to trains
+#'   on different tracks.}
+#'   \item{Line}{Two-letter abbreviation for the line (e.g.: RD, BL, YL, OR, GR,
+#'   or SV). May also be `NA` for trains with no passengers.}
+#'   \item{LocationCode}{Station code for where the train is arriving. Useful
+#'   when passing in All as the `StationCodes` parameter. Use this value in
+#'   other rail-related APIs to retrieve data about a station.}
+#'   \item{LocationName}{Full name of the station where the train is arriving.
+#'   Useful when passing in "All" as the `StationCodes` parameter.}
+#'   \item{Min}{Minutes until arrival. Can be a numeric value, 0 (arriving),
+#'   -1 (boarding), or `NA`.}
 #' }
-#' @param station Character vector of station names or `NULL` for all.
+#' @param StationCodes Character vector of station codes. For all predictions,
+#'   use `NULL` (default) or "All".
+#' @examples
+#' \dontrun{
+#' next_train(StationCodes = c("A02", "B02"))
+#' }
+#' @return Data frame of train arrivals
+#' @seealso <https://developer.wmata.com/docs/services/547636a6f9182302184cda78/operations/547636a6f918230da855363f>
+#' @family Real-Time Predictions
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
 #' @export
-next_train <- function(station = NULL) {
-  if (all(is.null(station))) {
-    station <- "All"
+next_train <- function(StationCodes = NULL) {
+  if (all(is.null(StationCodes))) {
+    StationCodes <- "All"
   }
   json <- wmata_api(
     type = "StationPrediction",
-    endpoint = paste("GetPrediction", paste(station, collapse = ","), sep = "/")
+    endpoint = paste(
+      "GetPrediction",
+      paste(StationCodes, collapse = ","),
+      sep = "/")
   )
-  df <- jsonlite::fromJSON(json, flatten = TRUE)[[1]]
-  if (length(df) == 1) {
+  dat <- jsonlite::fromJSON(json, flatten = TRUE)[[1]]
+  if (length(dat) == 1) {
     warning("no next trains at this station")
     return(empty_train)
   }
-  df <- df[, c(7:9, 3:6, 1)]
-  df <- tibble::add_column(df, Order = seq(nrow(df)), .before = 1)
-  names(df) <- names(empty_train)
-  mins <- df$min
-  mins[mins == "ARR"] <- 0
-  mins[mins == "BRD"] <- -1
-  mins[mins == "---" | mins == ""] <- NA
-  df$min <- as.integer(mins)
-  df$cars[df$cars == "-"] <- NA
-  df$cars <- as.integer(df$cars)
-  df$group <- as.integer(df$group)
-  tibble::as_tibble(df)
+  dat <- tibble::add_column(dat, Order = seq(nrow(dat)), .before = 1)
+  dat$Min[dat$Min == "ARR"] <- 0
+  dat$Min[dat$Min == "BRD"] <- -1
+  dat$Min[dat$Min == "---" | dat$Min == ""] <- NA
+  dat$Car[dat$Car == "-"] <- NA
+  dat$Line[dat$Line == "No"] <- NA
+  dat <- utils::type.convert(dat, as.is = TRUE)
+  tibble::as_tibble(dat)
 }
 
-empty_train <- data.frame(
-  stringsAsFactors = FALSE,
-  order = integer(),
-  station = character(),
-  name = character(),
-  min = integer(),
-  to_station = character(),
-  to_name = character(),
-  group = integer(),
-  line = character(),
-  cars = integer()
+empty_train <- tibble::tibble(
+  Order = integer(),
+  Car = integer(),
+  Destination = character(),
+  DestinationCode = character(),
+  DestinationName = character(),
+  Group = integer(),
+  Line = character(),
+  LocationCode = character(),
+  LocationName = character(),
+  Min = integer()
 )
