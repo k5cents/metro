a4202018444e0038f373b13b8df4a66217449447
Kiernan Nicholls <kiernann@protonmail.com>
2021-02-21 22:51:03 -0500

    Simplify key handling functions

--- R/key.R
+++ R/key.R
@@ -2,24 +2,36 @@
 #'
 #' @description
 #' All calls to the WMATA API must be accompanied by a personal API key. A free
-#' key can be obtained by subscribing to the default tier. See
-#' [browse_wmata_key()] for more information on getting a key and storing on
-#' your system.
+#' key can be obtained by subscribing to the default tier:
+#'
+#' 1. Sign up for a free WMATA [developer account][1].
+#' 2. Log in and subscribe to the free [default tier][2].
+#' 3. Copy the primary or secondary key from your [profile][3].
+#' 4. Pass your API key to a function through one of three ways:
+#'     1. Use [Sys.setenv()] to temporarily set define `WMATA_KEY`.
+#'     2. Pass your key as a string to the `api_key` argument of any function.
+#'     3. Store the the WMATA key as a line like `WMATA_KEY=xxxyyyzzz` in your
+#'     `.Renviron` file.
 #'
 #' The WMATA also provides a demonstration key. This key should **never** be
 #' used in production applications, it is rate limited and subject to change at
-#' _any_ time. The key can be found on
-#' [this page](https://developer.wmata.com/products/5475f236031f590f380924ff).
-#' If the user has the "rvest" package installed, the [wmata_demo()] function
-#' can be used to try scrape said page and look for the key automatically.
+#' _any_ time. The key can be found on the [WMATA products page][4] when not
+#' signed into a developer account. The [wmata_demo()] function can be used to
+#' try and scrape the demo key from this page automatically.
 #'
 #' The [wmata_validate()] function can be used to verify a key is valid.
 #'
+#' [1]: https://developer.wmata.com/signup/
+#' [2]: https://developer.wmata.com/products/5475f1b0031f590f380924fe
+#' [3]: https://developer.wmata.com/developer
+#' [4]: https://developer.wmata.com/products/5475f236031f590f380924ff
+#'
 #' @details
 #' Default tier sufficient for most casual developers. Rate limited to 10
-#' calls/second and 50,000 calls per day. This product contains 8 APIs:
+#' calls/second and 50,000 calls per day. This product contains 8 APIs and all
+#' APIs needed for the functions in this package:
 #' * Bus Route and Stop Methods
-#' * GTFS
+#' * GTFS (unused)
 #' * Incidents
 #' * Misc Methods
 #' * Rail Station Information
@@ -27,75 +39,58 @@
 #' * Real-Time Rail Predictions
 #' * Train Positions
 #'
+#' @param api_key Subscription key which provides access to this API. Found in
+#'   your [Profile](https://developer.wmata.com/developer). Defaults to calling
+#'   `Sys.getenv("WMATA_KEY")`.
+#' @return For [wmata_key()] and [wmata_demo()], a 32 character alphanumeric
+#' API key. For [wmata_validate()], either `TRUE` for a valid key or an
+#' error if invalid.
+#' @seealso <https://developer.wmata.com/docs/services/5923434c08d33c0f201a600a/operations/5923437c031f5914d0204bcf>
+#' @family Misc Methods
+#' @importFrom httr GET accept_json add_headers http_error content stop_for_status
 #' @export
-wmata_key <- function() {
-  api_key <- Sys.getenv("WMATA_KEY", "")
+wmata_key <- function(api_key = Sys.getenv("WMATA_KEY")) {
+  stopifnot(is.character(api_key), length(api_key) == 1)
   if (!nzchar(api_key)) {
-    warning("No WMATA API key found, see `browse_wmata_key()`.", call. = FALSE)
+    warning("No WMATA API key found, see help(wmata_key)", call. = FALSE)
   }
   return(api_key)
 }
 
 #' @rdname wmata_key
 #' @export
-wmata_demo <- function() {
-  if (is_installed("rvest")) {
-    a <- "https://developer.wmata.com/products/5475f236031f590f380924ff"
-    tryCatch(
-      error = function(e) {
-        warning("No API key scraped, returning empty string.", call. = FALSE)
-        return("")
-      },
-      expr = {
-        b <- httr::content(httr::GET(a))
-        rvest::html_text(rvest::html_nodes(b, ".bg-primary"))
-      }
-    )
-  } else {
-    stop("The 'rvest' package is needed to scrape demo API key.", call. = FALSE)
-  }
-}
-
-#' @rdname wmata_key
-#' @param api_key Subscription key which provides access to this API. Found in
-#'   your Profile. Defaults to [wamta_key()].
-#' @export
 wmata_validate <- function(api_key = wmata_key()) {
-  !httr::http_error(
-    x = httr::GET(
-      url = "https://api.wmata.com/Misc/Validate",
-      httr::add_headers(
-        `api_key` = api_key,
-        `Content-Type` = "application/json",
-        `Accept` = "application/json"
-      )
+  val <- httr::GET(
+    url = "https://api.wmata.com/Misc/Validate",
+    httr::accept_json(),
+    httr::add_headers(
+      `api_key` = api_key
     )
   )
+  if (httr::http_error(val)) {
+    msg <- httr::content(val)
+    httr::stop_for_status(msg$statusCode, msg$message)
+  } else {
+    TRUE
+  }
+
+  return(TRUE)
 }
 
 #' @rdname wmata_key
 #' @export
-browse_wmata_key <- function() {
-  if (is_installed("usethis")) {
-    w <- "https://developer.wmata.com/products/5475f236031f590f380924ff"
-    x <- "https://developer.wmata.com/signup/"
-    y <- "https://developer.wmata.com/products/5475f1b0031f590f380924fe"
-    z <- "https://developer.wmata.com/developer"
-    ui_url <- function(x) crayon::italic(crayon::blue(sprintf("<%s>", x)))
-    usethis::ui_info("Demo key available from {ui_url(w)} \\
-                     or {usethis::ui_code('wmata_demo()')}")
-    usethis::ui_line("===== or ====")
-    usethis::ui_todo("Sign up for a WMATA developer account {ui_url(x)}")
-    usethis::ui_todo("Subscribe to the free default tier {ui_url(y)}")
-    usethis::ui_todo("Copy the {usethis::ui_field('Primary Key')} \\
-                     from your profile {ui_url(z)}")
-    usethis::ui_todo("Call {usethis::ui_code('usethis::edit_r_environ()')} \\
-                     to open {usethis::ui_path('.Renviron')}.")
-    usethis::ui_todo("Store your WMATA key with a line like:")
-    usethis::ui_code_block("WMATA_KEY=xxxyyyzzz")
-    usethis::ui_todo("Make sure {usethis::ui_value('.Renviron')} ends with \\n")
-    invisible()
-  } else {
-    stop("The 'usethis' is package needed for key instructions", call. = FALSE)
-  }
+wmata_demo <- function() {
+  tryCatch(
+    error = function(e) {
+      warning("No API key scraped, returning an empty string.", call. = FALSE)
+      return("")
+    },
+    expr = {
+      a <- httr::GET(
+        url = "https://developer.wmata.com/products/5475f236031f590f380924ff"
+      )
+      b <- httr::content(x = a, as = "text")
+      regmatches(b, m = gregexpr("[a-z0-9]{32}", text = b))[[1]]
+    }
+  )
 }
