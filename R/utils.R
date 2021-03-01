b9f3eb805cf4df632033d6e84f96ed7bcb168452
Kiernan Nicholls <kiernann@protonmail.com>
2021-03-01 12:42:15 -0500

    Add no_data_now() for mocking

--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,14 @@ rows_bind <- function(list, list_names, col_name = "rowname", ...) {
     out
   }
 }
+
+#' Check if data has length and rows
+#'
+#' @param x A data frame or list to check
+#' @examples
+#' no_data_now(data.frame())
+#' no_data_now(data.frame(a = 1))
+#' @export
+no_data_now <- function(x) {
+  length(x) == 0 || (is.data.frame(x) & nrow(x) == 0)
+}
